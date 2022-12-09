6cae3f4f58fb0526440aa4cbdcf43cb17806e9b6
Yoni Weill <joweill@icloud.com>
2022-12-09 19:38:57 +0200

    updates

--- .config/jupyter/jupyter_notebook_config.py
+++ .config/jupyter/jupyter_notebook_config.py
@@ -1,5 +1,10 @@
 # pylint: disable=undefined-variable
 
-c.NotebookApp.contents_manager_class = 'jupytext.TextFileContentsManager'
+_CM_CLASS = 'jupytext.TextFileContentsManager'
+if hasattr(c.ServerApp, 'contents_manager_class'):
+    c.ServerApp.contents_manager_class = _CM_CLASS
+else:
+    c.NotebookApp.contents_manager_class = _CM_CLASS
+del _CM_CLASS
 # Use the percent format when saving as py
 c.ContentsManager.preferred_jupytext_formats_save = 'py:percent'
