ad1ec54e7c56db65c5639f0114a20a393c18e0f5
Yoni Weill <joweill@icloud.com>
2023-01-12 14:33:23 +0200

    updates

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -1920,7 +1920,7 @@ monitor-slow-pings() {
         if ((s == 0)); then
           echo '100 fast pings'
         elif ((c % 100 != 0)); then
-          printf '%d fast pings' $((c%100))
+          printf '%d fast pings before error\n' $((c%100))
         fi
       fi
       if ((s == 124)); then
