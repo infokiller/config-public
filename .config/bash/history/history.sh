27ddc83db2258dad6946c9494e0208d05ccb5312
Yoni Weill <joweill@icloud.com>
2023-11-23 17:09:13 +0200

    shell history: move preview script to separate file

--- .config/bash/history/history.sh
+++ .config/bash/history/history.sh
@@ -16,43 +16,6 @@ histcat-list-decode() {
   cut -c 23- | sed -r 's/↵/\n/g'
 }
 
-# TODO: Consider using a separate preview process that communicates with the
-# preview script using a fifo file, which should improve performance by
-# eliminating any latency in starting the preview process. For an example see:
-# ~/.config/ipython/profile_default/startup/10-keybindings.py
-IFS='' read -r -d '' _HISTCAT_FZF_PREVIEW_SCRIPT << 'EOF'
-  lines=({+})
-  highlight_cmd=(bat --color=always --paging=never '--wrap=character' 
-    "--terminal-width=${FZF_PREVIEW_COLUMNS}" '--language=sh' '--highlight-line=4')
-  is_multi_select=$((${#lines[@]} > 1))
-  if ((is_multi_select)); then
-    highlight_cmd+=(--style=grid,header)
-  else
-    highlight_cmd+=(--style=plain)
-  fi
-  for line in "${lines[@]}"; do
-    matches=()
-    while IFS='' read -r match; do
-      matches+=("${match}")
-    done < <(rg --text --context=3 --fixed-strings "${line}" < "${histcat_list_output_path}")
-    if ((${#matches[@]} == 0)); then
-      echo "ERROR: no matches for line: ${line}"
-      continue
-    fi
-    # When there are multiple lines we need to match, print the datetime of the first
-    # line so we can see the context.
-    extra_args=()
-    if ((is_multi_select)); then
-      first_match="${matches[@]:0:1}"
-      extra_args+=('--file-name' "${first_match:0:20}")
-      # printf '# %s\n' "${first_match:0:20}"
-    fi
-    printf '%s\n' "${matches[@]}" |
-      cut -c 23- |
-      "${highlight_cmd[@]}" "${extra_args[@]}"
-  done 
-EOF
-
 _get_histcat_multihosts_cmd() {
   local this_hostname
   this_hostname="$(get_hostname)"
@@ -75,17 +38,17 @@ histcat-select() {
   local XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
   local histcat_cache_dir="${XDG_CACHE_HOME}/histcat"
   mkdir -p -- "${histcat_cache_dir}"
-  local histcat_list_output_path="${histcat_cache_dir}/fzf_preview_$$"
+  local cache_file="${histcat_cache_dir}/fzf_preview_$$"
   # NOTE: The path variable in trap must be expanded when we run trap because if
   # it runs on EXIT the variable will not be defined.
   # shellcheck disable=SC2064
-  trap "\rm -- '${histcat_list_output_path}' &> /dev/null || true" ERR INT HUP EXIT TERM
-  local preview_script
-  preview_script="$(printf 'histcat_list_output_path=%q\n%s' "${histcat_list_output_path}" "${_HISTCAT_FZF_PREVIEW_SCRIPT}")"
+  trap "\rm -- '${cache_file}' &> /dev/null || true" ERR INT HUP EXIT TERM
+  local preview_script="${REPO_ROOT}/.config/bash/history/fzf-preview"
   local selector=(
     'fzf-tmux' '-p' '80%' '--' '--scheme=history' '--reverse' '--no-sort' '--multi'
     '-n3..,..' '--ansi' '--bind=ctrl-r:toggle-sort' '--exact'
-    "--preview=${preview_script}" '--preview-window=up:50%:hidden'
+    "--preview=$(printf '%s %q {+}' "${preview_script}" "${cache_file}")" 
+    '--preview-window=up:50%:hidden'
     '--bind=ctrl-t:toggle-preview'
   )
   local histcat_cmd=()
@@ -98,10 +61,10 @@ histcat-select() {
   ("${histcat_cmd[@]}" list --max-entries 500000 |
     # Remove escape characters when piping to the file caching the list, otherwise
     # searching for a line that originally had newlines will fail.
-    tee >(_remove_escape_chars > "${histcat_list_output_path}") |
+    tee >(_remove_escape_chars > "${cache_file}") |
     "${selector[@]}" "$@" |
     histcat-list-decode) || s=$?
-  \rm -- "${histcat_list_output_path}" &> /dev/null
+  \rm -- "${cache_file}" &> /dev/null
   # Delete files older than a week in case a previous cleanup was missed
   # (process was killed with -9, power loss, etc.).
   \find "${histcat_cache_dir}" -type f -mtime +7 -exec rm {} \;
